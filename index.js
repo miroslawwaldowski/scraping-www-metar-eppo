282aa322593e6d23b2e0e254e29d238a0fc5fe1f
Mirosław Waldowski <mwa@amu.edu.pl>
2021-01-11 16:42:12 +0100

    test6

--- index.js
+++ index.js
@@ -14,53 +14,43 @@ var logger = fs.createWriteStream("log.txt", {
   flags: "a", // 'a' means appending (old data will be preserved)
 });
 
-function readWeb() {
-  console.log("readWeb");
-  puppeteer
-    .launch()
-    .then(function (browser) {
-      return browser.newPage();
-    })
-    .then(function (page) {
-      return page.goto(url).then(function () {
-        return page.content();
-      });
-    })
-    .then(function (html) {
-      console.log("-------------------------");
-      $(".c1b > div:nth-child(5)  > b:nth-child(1)", html).each(function () {
-        var delta = Number($(this).text());
-        var time = new Date(Date.now() - delta * 60000).toISOString();
-        logger.write("");
-        logger.write(time + " ; ");
-      });
-      $(".c1b > div:nth-child(7)  > b:nth-child(1)", html).each(function () {
-        console.log($(this).text());
-        logger.write($(this).text() + " ; ");
-      });
-      $(".c1b > div:nth-child(8)  > b:nth-child(1)", html).each(function () {
-        console.log($(this).text());
-        logger.write($(this).text() + " ; ");
-      });
-      $(".c1b > div:nth-child(9)  > b:nth-child(1)", html).each(function () {
-        console.log($(this).text());
-        logger.write($(this).text() + " \n");
-      });
-
-      console.log(time2);
-    })
-    .catch(function (err) {
-      //handle error
-    });
-}
-
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/log.txt");
   console.log("get file");
 });
-
 setInterval(readWeb, 1 * 60000);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`server has started on port ${process.env.PORT}`);
 });
+
+async function readWeb() {
+  console.log("readWeb");
+  const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: null,
+    args: ["--incognito", "--no-sandbox", "--single-process", "--no-zygote"],
+  });
+  const page = await browser.newPage();
+  await page.goto(url);
+  const html = await page.content();
+  $(".c1b > div:nth-child(5)  > b:nth-child(1)", html).each(function () {
+    var delta = Number($(this).text());
+    var time = new Date(Date.now() - delta * 60000).toISOString();
+    logger.write("");
+    logger.write(time + " ; ");
+  });
+  $(".c1b > div:nth-child(7)  > b:nth-child(1)", html).each(function () {
+    console.log($(this).text());
+    logger.write($(this).text() + " ; ");
+  });
+  $(".c1b > div:nth-child(8)  > b:nth-child(1)", html).each(function () {
+    console.log($(this).text());
+    logger.write($(this).text() + " ; ");
+  });
+  $(".c1b > div:nth-child(9)  > b:nth-child(1)", html).each(function () {
+    console.log($(this).text());
+    logger.write($(this).text() + " \n");
+  });
+  browser.close();
+}
